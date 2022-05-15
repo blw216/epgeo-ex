51d573fd39593e9c2c24d7918c5d38d0d535a082
brady <blawoods@icloud.com>
2022-05-15 08:13:27 -0700

    add example data for user 179

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -6,14 +6,14 @@ import pandas as pd
 
 # Read in the trajectory data
 user000 = pd.read_csv('../example_data/data_000_track.csv')
-user010 = pd.read_csv('../example_data/data_010_track.csv')
-print(f"User 000: (Rows: {user000.shape[0]}) | User 010: (Rows: {user010.shape[0]})")
+user179 = pd.read_csv('../example_data/data_179_track.csv')
+print(f"User 000: (Rows: {user000.shape[0]}) | User 010: (Rows: {user179.shape[0]})")
 
 # Build the spatial index on the larger of the two data sets
-user010_points = list(zip(user010.longitude, user010.latitude))
-user010_sindex = NearestNeighbor(user010_points).build_index()
+user179_points = list(zip(user179.longitude, user179.latitude))
+user179_sindex = NearestNeighbor(user179_points).build_index()
 
 # For each point in user000, find the nearest point in user010
 user000['points'] = list(zip(user000.longitude, user000.latitude))
-user000['user010_nn'] = user000.points.apply(lambda x: user010_sindex.search_index(x))
+user000['user179_nn'] = user000.points.apply(lambda x: user179_sindex.search_index(x))
 print(user000.head())
